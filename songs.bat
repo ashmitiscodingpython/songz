baa8e2f90e3499abe648041309a0f8674c437133
ashmitiscodingpython <ashmit.jeevan@gmail.com>
2026-08-14 23:20:44 +0530

    Made sure that it doesn't ever accidentally open in Command Prompt (the old one) instead of Windows Terminal

--- songs.bat
+++ songs.bat
@@ -1,4 +1,4 @@
 @echo off
 set dire=%cd%
-start "songzplayer" cmd /c "title songzplayer && cd /d %~dp0 && python -X importtime songs.py 2> importtime.txt \"%dire%\""
+start "songzplayer" "Windows Terminal" /c "title songzplayer && cd /d %~dp0 && python -X importtime songs.py 2> importtime.txt \"%dire%\""
 exit
